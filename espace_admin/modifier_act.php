8ac7b291f80ab01ef2799b9921cfa853cc3caa2d
JennaBHA <benhadj.jenna@gmail.com>
2023-09-27 20:28:37 +0200

    rajout

--- espace_admin/modifier_act.php
+++ espace_admin/modifier_act.php
@@ -1,8 +1,46 @@
 <?php
 $bdd = new PDO('mysql:host=localhost;dbname=manif;', 'root', '');
-if(isset($_GET['id']) AND !empty($_GET['id'])){
+if(isset($_GET['id']) && !empty($_GET['id'])){
+    $getid = $_GET['id'];
 
+    $recupAct = $bdd->prepare('SELECT * FROM activite WHERE id = ?');
+    $recupAct->execute(array($getid));
+    if($recupAct->rowCount() > 0){
+        $activiteInfo = $recupAct->fetch();
+        $titre = $activiteInfo['titre'];
+        $description = str_replace('<br />', '', $activiteInfo['description']);
+
+        if(isset($_POST['valider'])){
+            $titre_saisi = htmlspecialchars($_POST['titre']);
+            $description_saisi = nl2br(htmlspecialchars($_POST['description']));
+
+            $updateAct = $bdd->prepare('UPDATE activite SET titre = ?, description = ? WHERE id = ?');
+            $updateAct->execute(array($titre_saisi, $description_saisi, $getid));
+            header('Location: activite.php');
+        }
+
+    }else{
+        echo "Aucun article trouvé";
+    }
 }else{
-    echo "Aucun identifiant trouver";
+    echo "Aucun identifiant trouvé";
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Modifier l'article</title>
+</head>
+<body>
+    <form method="POST" action="">
+        <input type="text" name="titre" value="<?= $titre; ?>">
+        <br>
+        <textarea name="description"><?= $description ?></textarea>
+        <br><br>
+        <input type="submit" name="valider">
+    </form>
+</body>
+</html>
