41035e74de7e2d1a8297352b0b179456f7344160
JennaBHA <benhadj.jenna@gmail.com>
2023-10-19 17:23:19 +0200

    rangement

--- View/espace_admin/update/publier_act.php
+++ View/espace_admin/update/publier_act.php
@@ -1,19 +1,38 @@
 <?php
 session_start();
-$bdd = new PDO('mysql:host=localhost;dbname=manif;', 'root', '');
-if(!$_SESSION['mdp']){
-    header('Location: ../espace_admin/connexion/connexion.php');
+
+try {
+    $bdd = new PDO('mysql:host=localhost;dbname=manif;', 'root', '');
+} catch (PDOException $e) {
+    die('Connection failed: ' . $e->getMessage());
 }
 
-if(isset($_POST['envoie'])) {
-    if(!empty($_POST['titre']) AND !empty($_POST['description'])) { 
+if (!$_SESSION['mdp']) {
+    header('Location: ../espace_admin/connexion/connexion.php');
+    exit;
+}
+
+$message = '';
+$responsables = array(); // Créez un tableau vide pour stocker la liste des responsables.
+
+// Récupérez la liste des responsables depuis la base de données.
+$requeteResponsables = $bdd->query('SELECT nom FROM responsable');
+while ($responsable = $requeteResponsables->fetch()) {
+    $responsables[] = $responsable;
+}
+
+if (isset($_POST['envoie'])) {
+    if (!empty($_POST['titre']) && !empty($_POST['description'])) {
         $titre = htmlspecialchars($_POST['titre']);
         $description = nl2br(htmlspecialchars($_POST['description']));
 
-        $inserAct = $bdd->prepare('INSERT INTO activite(titre, description) VALUES(?, ?)');
-        $inserAct->execute(array($titre, $description));
+        // Ajoutez votre code pour récupérer la valeur sélectionnée dans le champ "responsable".
+        $responsable = $_POST['responsable'];
 
-        $message = '<p style="color: green;">L\'activité a bien été envoyé</p>';
+        $inserAct = $bdd->prepare('INSERT INTO activite(titre, description, responsable) VALUES(:titre, :description, :responsable)');
+        $inserAct->execute(array(':titre' => $titre, ':description' => $description, ':responsable' => $responsable));
+
+        $message = '<p style="color: green;">L\'activité a bien été envoyée</p>';
     } else {
         $message = '<p style="color: red;">Veuillez compléter tous les champs</p>';
     }
@@ -28,28 +47,37 @@ if(isset($_POST['envoie'])) {
     <link rel="stylesheet" href="../../style/style_admin/publier_act.css">
     <link rel="stylesheet" href="../../style/general/bouton.css">
     <title>Publier une activité</title>
+
 </head>
 <body>
-    <form method="POST" action="">
+    <div class="card">
         <h1>Publier une activité</h1>
-        <input type="text" name="titre" placeholder="Titre">
-        <br>
-        <textarea name="description" placeholder="Description"></textarea>
-        <br>
-        <input type="time" name="creneau" autocomplete="off" placeholder="Créneau">
-        <br><br>
-        <div class="button-container">
-            <button class="button" type="submit" name="envoie">Envoie</button>
-            <a href="../index.php" class="button" id="btn">Retour</a>
-        </div>
-    </form>
+        <form method="post">
+            <input type="text" name="titre" placeholder="Titre">
+            <br><br>
+            <textarea name="description" placeholder="Description"></textarea>
+            <br>
+            <h4>Responsable : </h4>
+            <select name="responsable" id="monselect" placeholder="Responsable">
+                <?php foreach ($responsables as $responsable) : ?>
+                    <option value="<?php echo $responsable['nom']; ?>"><?php echo $responsable['nom']; ?></option>
+                <?php endforeach; ?>
+            </select>
+            <br><br>
+            <input type="time" name="creneau" autocomplete="off" placeholder="Créneau">
+            <br><br>
+            <div class="button-container">
+                <button class="button" type="submit" name="envoie">Envoie</button>
+                <a href="../index.php" class="button" id="btn">Retour</a>
+            </div>
+        </form>
+    </div>
     
-    <button class="button1" role="button">Activité -&gt;</button>
-
+    <a href="../activite/activite.php" class="button1" role="button">Activité -&gt;</a>
     <!-- Message container -->
     <div id="message-container">
         <?php
-        if(isset($message)) {
+        if (!empty($message)) {
             echo $message;
         }
         ?>
